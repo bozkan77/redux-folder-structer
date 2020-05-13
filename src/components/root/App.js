d9661c2b0e66a7981bc429ffe3c0e89eacd83873
unknown <brk.ozzkan@gmail.com>
2020-05-13 13:07:07 +0300

    Add react and redux folder structure

---
+++ src/components/root/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Dashboard from './Dashboard';
+import Navi from '../navigation/Navi';
+
+function App() {
+  return (
+    <div className='App'>
+      <Navi />
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
